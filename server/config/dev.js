c5d43c07c7c4d6ee9b2f63370fc3f24845681946
MarcLegaspi <63316236+MarcLegaspi@users.noreply.github.com>
2020-07-17 11:47:47 +0800

    Delete dev.js
    Removed dev.js

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-
-module.exports = {
-    DB_URI: 'mongodb://test-user:1234Qwer@cluster0-shard-00-00.pfvtj.mongodb.net:27017,cluster0-shard-00-01.pfvtj.mongodb.net:27017,cluster0-shard-00-02.pfvtj.mongodb.net:27017/bwm-new?ssl=true&replicaSet=atlas-38nz73-shard-0&authSource=admin&retryWrites=true&w=majority'
-}
